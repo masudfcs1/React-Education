c4267124a172801bef121e647c4ac2157fc25059
masudfcs1 <masudfcs1@gmail.com>
2023-07-24 20:24:10 +0600

    Updated

---
+++ src/components/about/AboutCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutCard = () => {
+  return <div>AboutCard</div>;
+};
+
+export default AboutCard;
--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./hero.css";
+import Title from "../../common/title/Title";
 const Hero = () => {
   return (
     <>
       <section className="hero">
         <div className="container">
           <div className="row">
+            <Title
+              subtitle="WELCOME TO ACDAEMIA"
+              title="BEST ONLINE EDUCATION"
+            />
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
               totam qui voluptate similique quae et aliquam aliquid error. Natus
-              nihil vero reiciendis accusamus voluptatem! Debitis provident
-              quidem itaque ut eius.
+              nihil vero reiciendis accusamus.
             </p>
 
             <div className="button">
@@ -18,7 +22,7 @@ const Hero = () => {
                 GET STARTED NOW <i className=" fa fa-long-arrow-alt-right "></i>{" "}
               </button>
 
-              <button className="primary-btn">
+              <button>
                 VIEW COURSE <i className=" fa fa-long-arrow-alt-right "></i>{" "}
               </button>
             </div>
