ef5bad7c6896033204989eba560cc81a5a3771d7
masudfcs1 <masudfcs1@gmail.com>
2023-07-28 09:03:15 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/common/heading/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import CourseHome from "./components/allcouses/CourseHome";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           {" "}
           <Route path="/" exact component={Home} />{" "}
           <Route path="/about" exact component={About} />{" "}
+          <Route path="/courses" exact component={CourseHome} />{" "}
         </Switch>{" "}
       </Router>{" "}
     </>
---
+++ src/components/allcouses/CoursesCard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { coursesCard } from "../../dummydata";
+import "./courses.css";
+const CoursesCard = () => {
+  return (
+    <>
+      <section className="coursesCard">
+        <div className="container grid2">
+          {coursesCard.map((d) => (
+            <div className="items">
+              <div className="content flex">
+                <div className="left">
+                  <div className="img">
+                    <img src={d.cover} alt="" />
+                  </div>
+                </div>
+              </div>
+              <div className="text">
+                <h1> {d.coursesName} </h1>
+                <div className="rate">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <label htmlFor="">(5.0)</label>
+                </div>
+                <div className="details">
+                  {d.courTeacher.map((details) => (
+                    <div className="box">
+                      <div className="dimg">
+                        <img src={details.dcover} alt="" />
+                      </div>
+                      <div className="para">
+                        <h4> {details.name} </h4>
+                      </div>
+
+                      <span> {details.totalTime}</span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CoursesCard;
